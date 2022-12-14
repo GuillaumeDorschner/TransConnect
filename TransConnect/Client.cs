57491b87fde9d4c696f5386d11c160ebf2e85569
Valentin Grateau <valentin.grateau1309@gmail.com>
2022-12-14 11:23:12 +0100

    Tris clients

--- TransConnect/Client.cs
+++ TransConnect/Client.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 namespace TransConnect
 {
-	public class Client : Personne
+	public class Client : Personne , ITris
 	{
 		//Collection de clients en mémoire = static List<Client> dans le main surement
 		protected List<Commande> commande;
+		public delegate int typeDeTri();
 
 		public Client( string nom, string prenom, DateTime naissance, string adressePostale, string adresseMail, int portable,
             List<Commande> commande) : base(nom, prenom, naissance, adressePostale, adresseMail, portable)
@@ -16,6 +17,42 @@ namespace TransConnect
 		{
 			get { return commande; }
 		}
-	}
+
+		public int achatsCumule()
+		{
+			int sum = 0;
+			foreach(Commande i in commande)
+			{
+				sum += i.Prix;
+			}
+			return sum;
+		}
+
+		public int Tri1(Client a, Client b)
+		{
+			int x = a.Nom.CompareTo(b.Nom);
+			if(x==0)
+			{
+				x = x + a.Prenom.CompareTo(b.Prenom);
+			}
+			return x;
+		}
+
+        public int Tri2(Client a, Client b)
+        {
+			return a.adressePostale.CompareTo(b.adressePostale);
+        }
+
+        public int Tri3(Client a, Client b)
+        {
+            return a.achatsCumule().CompareTo(b.achatsCumule());
+        }
+
+		public int typeDeTri1(ITris tri, Client a, Client b)
+		{
+			return Tri1(a, b);
+		}
+    }
 }
+
 
